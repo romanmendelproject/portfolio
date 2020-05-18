8184cea8f342e3bad509203c2806212c269d957c
Roman <romanmendel777@gmail.com>
2020-05-18 23:16:37 +0300

    1

--- src/header/Header.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import style from './Header.module.css';
-
-function Header() {
-  return <div className={style.header}>tgeetrgr</div>
-}
-
-export default Header;
---
+++ src/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from './Header.module.css';
+
+function Header() {
+  return <div className={styles.header}>tgeetrgr</div>
+}
+
+export default Header;
+
